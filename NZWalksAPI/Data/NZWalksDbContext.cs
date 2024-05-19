0a87da76460de2ac5ad50b3a00fe7870d28efa23
Nabi Nabili <nbilinbi7@gmail.com>
2024-05-19 13:08:21 +0200

    Create Web Api and Domain

---
+++ NZWalksAPI/Data/NZWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+
+        DbSet<Difficulty> Difficulties { get; set; }
+
+        DbSet<Walk> Walks { get; set; }
+
+        DbSet<Region> Regions { get; set; }
+    }
+}
